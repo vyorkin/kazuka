3ad9af92072ab03942961c52110ed78b55001c8b
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2025-07-27 14:56:18 +0300

    WIP

--- crates/strategies/kazuka-mev-share/src/executor.rs
+++ crates/strategies/kazuka-mev-share/src/executor.rs
@@ -1,40 +1,20 @@
-use alloy::signers::Signer;
+use std::sync::Arc;
+
+use alloy::{network::AnyNetwork, providers::DynProvider, signers::Signer};
 use jsonrpsee::http_client::{
     HttpClient, HttpClientBuilder,
     transport::{self},
 };
-use mev_share::rpc::{FlashbotsSigner, FlashbotsSignerLayer, MevApiClient};
+use mev_share::rpc::{FlashbotsSignerLayer, MevApiClient};
 
 pub struct MevShareExecutor {
-    client: Box<dyn MevApiClient + Send + Sync>,
+    provider: Arc<DynProvider<AnyNetwork>>,
 }
 
 impl MevShareExecutor {
-    pub fn new(signer: impl Signer + Clone + 'static) -> Self {
-        // let provider
-
-        todo!()
+    pub fn new(provider: Arc<DynProvider<AnyNetwork>>) -> Self {
+        Self { provider }
     }
 }
 
-// impl MevShareExecutor {
-//     pub fn new(signer: impl Signer + Clone + 'static) -> Self {
-//         let signer_layer = FlashbotsSignerLayer::new(signer);
-//         let service_builder = tower::ServiceBuilder::new()
-//             .map_err(transport::Error::Http)
-//             .layer(signer_layer);
-//
-//         let http =
-//
-//         // HttpClient::builder().set_rpc_middleware(service_builder);
-//
-//         // .build("https://relay.flashbots.net:443")
-//         // .unwrap();
-//
-//         // .set_http_middleware(service_builder);
-//         // .build("https://relay.flashbots.net:443")
-//         // .expect("failed to build HTTP client");
-//
-//         todo!()
-//     }
-// }
+impl Executor<SendBundleRequest> for MevShareExecutor {}
